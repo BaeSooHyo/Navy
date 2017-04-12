72670bffe4a22e77002e45895567d5b7d73b832d
ThibaudJeannin <thibaud.jeannin@gmail.com>
2017-04-12 12:14:27 +0200

    ajout de structures et de fonctions de manipulation de structures

--- navy.c
+++ navy.c
@@ -5,14 +5,26 @@
 #include <stdbool.h>
 
 #define BUFSIZE 256
+#define MAPSIZE 10
+
+#define MINE 50
+
+/*
+Grille 10x10
+x : A-J
+y : 0-9
+5 mines
+5 navires
+3 actions : SHOOT POLL MOVE
+*/
 
 enum boatType
 {
-  PorteAvions,
-  Croiseur,
-  ContreTorpilleur,
-  SousMarin,
-  Torpilleur
+  PorteAvions,          //5
+  Croiseur,             //4
+  ContreTorpilleur,     //3
+  SousMarin,            //3
+  Torpilleur            //2
 };
 
 struct boat
@@ -30,20 +42,44 @@ struct coord
 
 struct map
 {
-  int i;
+  int map[MAPSIZE][MAPSIZE];
+/*
+Valeurs :
+  0 : nul
+  10 : tiré vide
+  20 : tiré touché
+  30 : scanné vide
+  40 : scanné detecté
+  50 : miné
+
+
+
+*/
 };
+
+
+void map_create(struct map *self);
+void set_coord(struct coord *self, int x, int y);
+void move_coord(struct coord *self, int x, int y);
+void set_mine(struct map *self, int x, int y);
+void send_coord_explicit(int x, int y);
+void send_coord(struct coord *self);
 
 int main(int argc, char const *argv[]) {
   setbuf(stdout, NULL);
 
   char buffer[BUFSIZE];
+  struct coord radar;
+  set_coord(&radar, 2, 2);
+  struct map ennemy;
+  map_create(&ennemy);
 
   // mines
-  printf("B2\n");
-  printf("D0\n");
-  printf("C4\n");
-  printf("C8\n");
-  printf("E5\n");
+  set_mine(&ennemy, 2, 2);
+  set_mine(&ennemy, 4, 0);
+  set_mine(&ennemy, 3, 4);
+  set_mine(&ennemy, 3, 8);
+  set_mine(&ennemy, 5, 5);
 
   // ships
   printf("B2B6\n");
@@ -57,15 +93,69 @@ int main(int argc, char const *argv[]) {
   printf("I0I1\n");
   fgets(buffer, BUFSIZE, stdin);
 
-  for (;;)
+
+
+  for (size_t i = 0; ; i++)
   {
-    printf("SHOOT\n"); // or POOL or MOVE
+    printf("POLL\n"); // SHOOT or POLL or MOVE
     printf("D2\n");
     fgets(buffer, BUFSIZE, stdin);
     fgets(buffer, BUFSIZE, stdin); // NOTHING or ATTACK
+    //TODO Récupérer infos ici
   }
   return EXIT_SUCCESS;
 }
+
+void map_create(struct map *self)
+{
+  for (size_t i = 0; i < MAPSIZE; i++)
+  {
+    for(size_t j = 0; j < MAPSIZE; j++)
+    {
+      self->map[i][j] = 0;
+    }
+  }
+}
+
+void set_coord(struct coord *self, int x, int y)
+{
+  if ((0 <= x && x < 10)&&(0 <= y && y < 10))
+  {
+  self->x = x;
+  self->y = y;
+  }
+}
+
+void move_coord(struct coord *self, int x, int y)
+{
+  if ((0 <= self->x + x && self->x + x < 10)&&(0 <= self->y + y && self->y + y < 10))
+  {
+  self->x = x;
+  self->y = y;
+  }
+}
+
+void set_mine(struct map *self, int x, int y)
+{
+  if (!((0 <= x && x < 10)&&(0 <= y && y < 10)))
+  {
+    x = 2; y = 2;
+  }
+  self->map[x][y] = MINE;
+  send_coord_explicit(x,y);
+}
+
+void send_coord_explicit(int x, int y)
+{
+  char abscisse = 'A' + y;
+  printf("%c%d\n", abscisse, y);
+}
+
+void send_coord(struct coord *self)
+{
+  send_coord_explicit(self->x, self->y);
+}
+
 
 
 //PLAN
